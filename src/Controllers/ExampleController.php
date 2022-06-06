5ff923eaa047d9fa6f4873f59f84fbe6d86c5fb6
Sultanov Mirjamol <9625526+cljamal@users.noreply.github.com>
2022-06-07 01:19:16 +0500

    Update ExampleController.php

--- src/Controllers/ExampleController.php
+++ src/Controllers/ExampleController.php
@@ -1,18 +1,13 @@
 <?php
 
-namespace Dovcha\%SERVICE_NAME%\Controllers;
+namespace SultanovSolutions\%SERVICE_NAME%\Controllers;
 
-use Dovcha\%SERVICE_NAME%\Models\Example;
+use SultanovSolutions\%SERVICE_NAME%\Models\Example;
 use SultanovSolutions\LaravelBase\Controllers\BaseController;
-use Illuminate\Database\Eloquent\Builder;
-use SultanovSolutions\LaravelBase\Models\BaseModel as Model;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ExampleController extends BaseController
 {
-    protected mixed $model = Example::class;
+    protected ?string $model = Example::class;
 
     protected string $scope = 'example';
 }
